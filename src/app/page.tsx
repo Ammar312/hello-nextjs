636bd6e265db2ec2e4d9aafb8385ee373823e35f
Sheikh Ammar Ul Mustafa <ammar.ul.mustafa3@gmail.com>
2024-04-16 18:15:30 +0500

    whychooseus complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-import Image from "next/image";
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+    </main>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,32 @@
-import { cn } from "@/utilis/cn";
+"use client";
+import { cn } from "@/utils/cn";
 import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import Link from "next/link";
 const Navbar = ({ className }: { className?: string }) => {
+  const [active, setActive] = useState<string | null>(null);
   return (
     <div
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
-    ></div>
+    >
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+          <MenuItem setActive={setActive} active={active} item="Home" />
+        </Link>
+        <MenuItem setActive={setActive} active={active} item="All Courses">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">All Courses</HoveredLink>
+            <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/courses">Advanced Composition</HoveredLink>
+            <HoveredLink href="/courses">Songwriting</HoveredLink>
+            <HoveredLink href="/courses">Music Production</HoveredLink>
+          </div>
+        </MenuItem>
+        <Link href={"/contact"}>
+          <MenuItem setActive={setActive} active={active} item="Contact Us" />
+        </Link>
+      </Menu>
+    </div>
   );
 };
 
